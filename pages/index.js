bff7546d555d95ab1daceefc5d0e8e9e836d13d2
Ali Unwala <aliunwaladev@gmail.com>
2024-04-18 13:16:59 -0500

    Adding most of the tech bubbles in for sections that were missing them.

--- pages/index.js
+++ pages/index.js
@@ -92,14 +92,14 @@ export default function Home() {
                 description="With the goal of making all of IBM's products accessiblie, in this role I collaborated with design and content experts to build a cohesive product which helped 288k IBM employees make products available to people with disabilities 
                 <br></br>
                 (see the <a href='#projects'>projects</a> section for some of the work I did during this role <a href='#ableSite'>ibm.com/able</a> and <a href='#checker'>IBM Equal Access Accessibility Checker</a> to see some of my work). "
-                technologies="React,Node,Docker,Javascript,CSS,HTML,JSX,VSCode, Typescript,Node.js"
+                technologies="React,Node,Docker,Javascript,CSS,HTML,JSX,VSCode,Typescript,Node.js,CouchDB"
               ></JobCard>
               <JobCard
                 dates="2018-2020"
                 title="IBM Research: Senior Software Engineer"
                 titleLink="https://research.ibm.com/"
                 description="Architected and programmed automated code to scan html pages creating an event driven architecture which checked the accessibility on web pages"
-                technologies=""
+                technologies="Javascript,CSS,HTML,VSCode,Node.js"
               ></JobCard>
               <JobCard
                 dates="2015-2018"
@@ -109,7 +109,7 @@ export default function Home() {
                 Due to my direct technical contributions this team grew from initially 2 people to nearly 20 people working in parallel in the pursuit of cognitive embodiments actively supported by 2 IBM Fellows, Grady Booch and Rob High.
                 <br></br>(see the <a href='#projects'>projects</a> section for some of the work I did during this role  <a href='#hilton'>Robotic Concierge - IBM Watson x Hilton</a>, <a href='#intu'>IBM Project Intu</a>, and <a href='#chef'>IBM Chef Watson</a> to see some of my work)
                 "
-                technologies=""
+                technologies="REST,Python,MATLAB,Vim,Unix/Linux,CouchDB,Robotics Operating System,Point Cloud Library,Open Computer Vision (OpenCV),Watson Bluemix API,Nao Robot,Pepper Robot"
               ></JobCard>
               <JobCard
                 dates="2014-2014"
@@ -117,14 +117,14 @@ export default function Home() {
                 description="This course's primary objective was to get freshmen engineering and computer science students interested in robotics research, and introduce them to a real robotics system.
                 Class dealt heavily with the segbot robotics platform and taught basics from Robotic Operating System (ROS) all the way up to building a final project on the robot.
                 I taught around 40% of the lectures in the class. (The professor and myself took turns lecturing)"
-                technologies=""
+                technologies="Python,MATLAB,Vim,Unix/Linux,Robotics Operating System,Point Cloud Library,Open Computer Vision (OpenCV),Segbot Robot"
               ></JobCard>
               <JobCard
                 dates="2011-2014"
                 title="Intel Corporation: Graduate Technical Intern"
                 titleLink="https://www.intel.com/content/www/us/en/homepage.html"
                 description="Worked in pre-silicon validation, verifying Intel’s mobile processors (Atom, Celeron and Pentium) before they are sent for fabrication."
-                technologies=""
+                technologies="Java,C++,Assembly,C,Vim,Unix,TCL,Verilog,Perl,VHDL"
               ></JobCard>
             </section>
 
@@ -167,14 +167,13 @@ export default function Home() {
                 <a href='https://www.zdnet.com/article/ibm-rolls-out-tools-to-help-developers-ensure-apps-are-accessible/'>ZDNET</a>,
                  and several others."
                 image="/IBMbee2.png"
-                technologies="React,Node,Docker"
+                technologies="React,Node,Docker,Javascript,CSS,HTML,JSX,VSCode,Typescript,Node.js,CouchDB"
               ></ProjectCard>
               <ProjectCard
                 id="checker"
                 // dates="2020-2023"
                 title="IBM Equal Access Accessibility Checker"
                 titleLink="https://chromewebstore.google.com/detail/ibm-equal-access-accessib/lkcagbfjnkomcinoddgooolagloogehp?pli=1"
-                technologies=""
                 goal="This web extension allows users to automatically check for accessiblity issues on any websites they own. I contributed a dynamically drawn visualization that highlights accessibility bugs that a user has on their webpage.
                 <br></br>
                 This work was featured by 
@@ -183,13 +182,13 @@ export default function Home() {
                 <a href='https://www.csun.edu/cod/conference/sessions/2022/index.php/public/presentations/view/2348'>CSUN</a>
                 ."
                 image="/IBMchecker.png"
+                technologies="React,Node,Javascript,CSS,HTML,VSCode,Typescript,Node.js,Chrome Extension"
               ></ProjectCard>
               <ProjectCard
                 // dates="2020-2023"
                 id="hilton"
                 title="Robotic Concierge - IBM Watson x Hilton"
                 titleLink="http://fortune.com/2016/03/09/hilton-robot-ibm-watson/"
-                technologies=""
                 goal="Built a highly successful robotic concierge called “Connie” in partnership with Hilton Hotels. The robotic concierge is was operating at Hilton’s proving-ground for innovation in Washington DC. 
                 The robotic concierge, powered by Watson, is designed to help customers navigate the hotel and the surrounding area using natural language.
                 <br></br>
@@ -201,13 +200,13 @@ export default function Home() {
                 <a href='http://www.usatoday.com/story/travel/roadwarriorvoices/2016/03/09/introducing-connie-hiltons-new-robot-concierge/81525924/'>USA Today</a>,
                 <a href='http://time.com/4251412/ibm-hilton-robot-hotel/'>Time</a> and several others."
                 image="/IBMNao.webp"
+                technologies="REST,Python,Vim,Unix/Linux,CouchDB,Robotics Operating System,Point Cloud Library,Open Computer Vision (OpenCV),Watson Bluemix API,Nao Robot"
               ></ProjectCard>
               <ProjectCard
                 // dates="2020-2023"
                 id="intu"
                 title="IBM Project Intu"
                 titleLink="http://www.zdnet.com/article/ibms-watson-ai-could-soon-be-in-devices-from-pcs-to-robots-thanks-to-project-intu/"
-                technologies=""
                 goal="A framework for integration of IBM’s Artificial Intelligence services on to a multitude of different platforms, including robotics. Allowing any device with a CPU, microphone, and speakers to talk to Watson.
                 <br></br>
                 This work was featured by 
@@ -216,13 +215,13 @@ export default function Home() {
                 <a href='https://siliconangle.com/blog/2016/11/09/ibm-offers-developers-a-way-to-extend-watsons-capabilities-into-any-device/'>Silicon Angle</a>,
                 <a href='https://www.prnewswire.com/news-releases/ibm-launches-experimental-platform-for-embedding-watson-into-any-device-300359719.html'>PR News Wire</a>, and several others."
                 image="/IBMIntu.jpg"
+                technologies="REST,Python,CouchDB,Watson Bluemix API,Nao Robot,Pepper Robot"
               ></ProjectCard>
               <ProjectCard
                 // dates="2020-2023"
                 id="chef"
                 title="IBM Chef Watson"
                 titleLink="https://www.theguardian.com/technology/2016/jun/04/man-v-machine-robots-artificial-intelligence-cook-write"
-                technologies=""
                 goal="This application allows the robot to act as an active kitchen assistant that will go through ingredients with you and then create a dynamically generated recipe for you to cook. I worked with the team to get this code functional on the Nao robotics platform.
                 <br></br>
                 This work was featured by 
@@ -232,15 +231,16 @@ export default function Home() {
                 <a href='https://www.bonappetit.com/entertaining-style/trends-news/article/chef-watson-app'>bon appetit</a>,
                  and several others."
                 image="/IBMChef.avif"
+                technologies="REST,Python,Robotics Operating System,Watson Bluemix API,Nao Robot,Pepper Robot"
               ></ProjectCard>
               <ProjectCard
                 // dates="2020-2023"
                 id="segbot"
                 title="Building Wide Intelligence"
                 titleLink="https://repositories.lib.utexas.edu/items/efb21691-020e-484e-b27f-0ec020710f19"
-                technologies=""
                 goal="Created a coupled robotics/AI system that could traverse multiple floors in the building to deliver coffee to various individuals."
                 image="/bwi1.jpg"
+                technologies="Python,MATLAB,Vim,Unix/Linux,Robotics Operating System,Point Cloud Library,Open Computer Vision (OpenCV),Segbot Robot"
               ></ProjectCard>
             </section>
 
