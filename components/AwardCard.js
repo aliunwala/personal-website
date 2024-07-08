986c3ed45626847f8906833f1f2c67d8d7b187c6
Ali Unwala <aliunwala@gmail.com>
2024-07-08 17:02:53 -0500

    Added an award card component and made it reactive and added 4 awards I have gotten in the past.

---
+++ components/AwardCard.js
@@ -0,0 +1,24 @@
+import styles from "./AwardCard.module.css";
+
+export default function AwardCard(props) {
+  return (
+    <>
+      <div className={styles.awardCard}>
+        <div className={styles.awardCardContainer}>
+          {/* Left */}
+          <div className={styles.awardCardLeft}>
+            <p className={styles.awardCardTitle}>{props.title}</p>
+          </div>
+
+          {/* Middle Spacer */}
+          <div className={styles.awardCardMiddle}></div>
+
+          {/* Right */}
+          <div className={styles.awardCardRight}>
+            <p className={styles.awardCardBody}>{props.subtitle}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
