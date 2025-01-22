ba0e33d4438ed85ff131ab38ab9b1532ed58ab6d
Ali Unwala <>
2025-01-21 22:31:48 -0600

    Adding support for jobs that do not have a link. I remove hyperlinks from these items.

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,73 +1,99 @@
 import styles from "./JobCard.module.css";
 
-export default function JobCard(props: Job) {
+interface TitleWithLinkProps {
+  title: string;
+  titleLink: string;
+}
+function TitleWithLink({ title, titleLink }: TitleWithLinkProps) {
+  const noLink = (
+    <div className={[styles.jobCardTitle, styles.jobCardRow].join(" ")}>
+      <span>{title}</span>
+    </div>
+  );
+
+  const hasLink = (
+    <a
+      href={titleLink}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={[
+        styles.jobCardLink,
+        styles.jobCardTitle,
+        styles.jobCardRow,
+      ].join(" ")}
+    >
+      <span>{title}</span>
+      <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-5 h-5 jobCardSVG"
+          height="20px"
+          width="20px"
+          style={{ verticalAlign: "middle" }}
+        >
+          <path
+            fillRule="evenodd"
+            d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </span>
+    </a>
+  );
+
+  if (titleLink) {
+    return hasLink;
+  } else {
+    return noLink;
+  }
+}
+
+export default function JobCard({
+  id,
+  dates,
+  title,
+  titleLink,
+  description,
+  technologies,
+}: JobCardProps) {
   return (
     <>
-      <div
-        id={props.id === undefined ? "" : props.id}
-        className={styles.jobCard}
-      >
+      <div id={id === undefined ? "" : id} className={styles.jobCard}>
         <div className={[styles.jobCardLeft, "textXS"].join(" ")}>
           {/* Left side of card with dates */}
-          {props.dates}
+          {dates}
         </div>
         <div className={styles.jobCardRight}>
           {/* Setting up title of card with a link if it exists */}
-          {props.titleLink !== undefined ? (
+          {titleLink !== undefined ? (
             <div className={styles.jobCardRow}>
-              <a
-                href={props.titleLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={[
-                  styles.jobCardLink,
-                  styles.jobCardTitle,
-                  styles.jobCardRow,
-                ].join(" ")}
-              >
-                <span>{props.title}</span>
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="w-5 h-5 jobCardSVG"
-                    height="20px"
-                    width="20px"
-                    style={{ verticalAlign: "middle" }}
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.22 14.78a.75.75 0 0 0 1.06 0l7.22-7.22v5.69a.75.75 0 0 0 1.5 0v-7.5a.75.75 0 0 0-.75-.75h-7.5a.75.75 0 0 0 0 1.5h5.69l-7.22 7.22a.75.75 0 0 0 0 1.06Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-              </a>
+              <TitleWithLink {...{ title, titleLink }}></TitleWithLink>
             </div>
           ) : (
-            <span className={styles.jobCardTitle}>{props.title}</span>
+            <span className={styles.jobCardTitle}>{title}</span>
           )}
 
           {/* Adding back dates if employment when card is smaller than a specific size */}
           <div className={[styles.jobCardDatesSmall, "textXS"].join(" ")}>
-            {props.dates}
+            {dates}
           </div>
 
           {/* Main body of card */}
           <p
             className={styles.jobCardBody}
-            // dangerouslySetInnerHTML={{ __html: props.description }}
+            // dangerouslySetInnerHTML={{ __html: description }}
           >
-            {props.description}
+            {description}
           </p>
 
           {/* Adding relavant tech stack bubbles */}
-          {props.technologies !== undefined && props.technologies !== "" ? (
+          {technologies !== undefined && technologies !== "" ? (
             <div className={styles.jobCardTechStack}>
-              {props.technologies.split(",").map((element, i) => {
+              {technologies.split(",").map((element, i) => {
                 return (
-                  <div key={props.id + i} className={styles.jobCardTechItem}>
+                  <div key={id + i} className={styles.jobCardTechItem}>
                     {element}
                   </div>
                 );
--- lib/interfaces.tsx
+++ lib/interfaces.tsx
@@ -1,44 +1,44 @@
-interface Job {
-  dates: string
-  title: string
-  titleLink?: string
-  description: JSX.Element
-  id: string
-  technologies: string
+interface JobCardProps {
+  dates: string;
+  title: string;
+  titleLink?: string;
+  description: JSX.Element;
+  id: string;
+  technologies: string;
 }
 
 interface Project {
-  id: string
-  title: string
-  titleLink: string
-  goal: JSX.Element
-  image: string
-  imageAlt: string
-  technologies: string
+  id: string;
+  title: string;
+  titleLink: string;
+  goal: JSX.Element;
+  image: string;
+  imageAlt: string;
+  technologies: string;
 }
 
 interface Education {
-  school: string
-  degree: string
-  degreeLevel: string
-  department: string
-  GPA: string
-  graduated: string
-  research?: string
-  researchLink?: string
+  school: string;
+  degree: string;
+  degreeLevel: string;
+  department: string;
+  GPA: string;
+  graduated: string;
+  research?: string;
+  researchLink?: string;
 }
 interface Award {
-  title: string
-  subtitle: string
+  title: string;
+  subtitle: string;
 }
 interface NavElement {
-  name: string
-  href: string
+  name: string;
+  href: string;
 }
 interface PersonalInfo {
-  name: string
-  title: string
-  subtitle: string
-  email: string
-  resume: string
+  name: string;
+  title: string;
+  subtitle: string;
+  email: string;
+  resume: string;
 }
